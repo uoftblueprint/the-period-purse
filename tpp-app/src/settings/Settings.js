2b680f8f49210918bc1f5bd48c448b5a9f39ff3c
Kenneth Miura <kenneth.t.miura@gmail.com>
2021-11-10 16:57:28 -0800

    added switch on card on Settings page

--- tpp-app/src/settings/Settings.js
+++ tpp-app/src/settings/Settings.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Switch } from 'react-native';
+import {Card} from 'react-native-elements';
 
 export default function Settings () {
+    const [isEnabled, setIsEnabled] = useState(true)
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
+            <Card>
+                <Switch
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                />
+            </Card>
         </View>
     )
 }
