9b945d210792a6319a629863c23287e8d91a34d3
Helen Li <hallenljy@gmail.com>
2021-11-10 20:26:00 -0500

    Removed unnecessary console.log line

--- tpp-app/src/home/Calendar.js
+++ tpp-app/src/home/Calendar.js
@@ -5,9 +5,6 @@ import { CalendarList } from 'react-native-calendars';
 
 export default () => {
     return (<CalendarList
-        // Callback which gets executed when visible months change in scroll view. Default = undefined
-        onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
-        
         // Max amount of months allowed to scroll to the past. Default = 50
         pastScrollRange={12}
         
