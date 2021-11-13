5fd57f37f2f1f1739c36b12c2ee7b765a5fb4b6c
Edward Han <edwardhan@HP-Printer-19YM.local>
2021-11-13 15:48:42 -0500

    Fixed layering and testing positions

--- tpp-app/src/home/Calendar.js
+++ tpp-app/src/home/Calendar.js
@@ -6,36 +6,39 @@ import DropDownPicker from 'react-native-dropdown-picker'
 
 const Calendar = () => {
     
-    return (<CalendarList
-    // Max amount of months allowed to scroll to the past. Default = 50
-    pastScrollRange={12}
+    return (
+        <CalendarList
+        // Max amount of months allowed to scroll to the past. Default = 50
+        pastScrollRange={12}
 
-    // Max amount of months allowed to scroll to the future. Default = 50
-    futureScrollRange={12}
+        // Max amount of months allowed to scroll to the future. Default = 50
+        futureScrollRange={12}
 
-    // Enable or disable scrolling of calendar list
-    scrollEnabled={true}
+        // Enable or disable scrolling of calendar list
+        scrollEnabled={true}
 
-    // Enable or disable vertical scroll indicator. Default = false
-    showScrollIndicator={true}
+        // Enable or disable vertical scroll indicator. Default = false
+        showScrollIndicator={true}
 
-    theme={{
-        calendarBackground: '#ffffff',
-        // Sun Mon Tue Wed Thu Fri Sat Bar
-        textSectionTitleColor: '#000000',
-        todayTextColor: 'red',
-        dayTextColor: '#000000',
-        monthTextColor: 'red',
-        textDayFontFamily: 'Avenir',
-        textMonthFontFamily: 'Avenir',
-        textDayHeaderFontFamily: 'Avenir',
-        textDayFontWeight: '500',
-        textMonthFontWeight: '400',
-        textDayHeaderFontWeight: '800',
-        textDayFontSize: 10,
-        textMonthFontSize: 14,
-        textDayHeaderFontSize: 10
-    }}/>)
+        theme={{
+            calendarBackground: '#ffffff',
+            // Sun Mon Tue Wed Thu Fri Sat Bar
+            textSectionTitleColor: '#000000',
+            todayTextColor: 'red',
+            dayTextColor: '#000000',
+            monthTextColor: 'red',
+            textDayFontFamily: 'Avenir',
+            textMonthFontFamily: 'Avenir',
+            textDayHeaderFontFamily: 'Avenir',
+            textDayFontWeight: '500',
+            textMonthFontWeight: '400',
+            textDayHeaderFontWeight: '800',
+            textDayFontSize: 10,
+            textMonthFontSize: 14,
+            textDayHeaderFontSize: 10
+        }}
+        />
+    )
 }
 
 const DropDown = () => {
@@ -54,6 +57,7 @@ const DropDown = () => {
 
     return (
         <DropDownPicker
+            containerStyle={styles.dropdown}
             open={open}
             value={value}
             items={items}
@@ -67,12 +71,15 @@ const DropDown = () => {
 export default () => {
     return (
         <View>
-        <DropDown></DropDown>
-        <Calendar></Calendar>
+            <DropDown></DropDown>
+            <Calendar></Calendar>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    dropdown: {
+        position: 'absolute',
+        marginTop: 400,
+    }
 });
