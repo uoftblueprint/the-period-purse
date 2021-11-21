ba3e9eaf6e727a0ea023bf6e99266a721fcffa4e
Edward Han <edwardhan@HP-Printer-19YM.local>
2021-11-20 21:05:41 -0500

    Modified comments

--- tpp-app/src/home/CalendarNavigator.js
+++ tpp-app/src/home/CalendarNavigator.js
@@ -4,7 +4,8 @@ import CalendarScreen from './pages/CalendarScreen'
 import YearScreen from './pages/YearScreen'
 
 // Create a navigation stack that can be used to access other pages
-// Stack begins first page at CalendarScreen
+// Stack begins first page at CalendarScreen. This is done so that the global
+// tab bar on the bottom is kept when navigating within pages.
 const Stack = createNativeStackNavigator();
 export default function CalendarNavigator() {
     return (
