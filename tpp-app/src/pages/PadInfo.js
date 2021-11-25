08ab2486b9e152e5f40d7259149c4861e219e5cd
Annie <liuzr.annie@gmail.com>
2021-11-24 20:08:03 -0500

    TPP=00018: Creation of all the page js files

---
+++ tpp-app/src/pages/PadInfo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View, Image} from 'react-native';
+
+export default function PadsInfo() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Pads</Text>
+        </View>
+    )
+}
