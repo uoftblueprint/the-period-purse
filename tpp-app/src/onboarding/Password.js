654cb6d6f2e591a9e63b5701260782c13207beeb
yuemily24 <76456096+yuemily24@users.noreply.github.com>
2022-02-05 13:11:33 -0500

    TPP-00045: quick log vs. multiple log and Info screen bug fixes (#56)
    * Working notification prototype
    
    * Added required functionality
    
    * created new day component in calendar to meet specs
    
    * passing params between pages
    
    * Removed alert when notification is pushed
    
    * style fix for back button
    
    * toggle overlay screen when you click middle button
    
    * animate rotation for middle tab button
    
    * new LogMultipleDates screen and new SVGs
    
    * clean up code into component files, add comments
    
    * toggle overlay screen when you click middle button
    
    * animate rotation for middle tab button
    
    * new LogMultipleDates screen and new SVGs
    
    merge CalendarScreen and App.js conflicts
    
    * clean up code into component files, add comments
    
    clean up more code conflicts in App.js and CalendarScreen.js
    
    * add missing svg files, fix CalendarScreen bg color
    
    * fix: info navigation working and naming warnings
    
    * refactored code, fixed some styling spacing
    
    * fix: circular dependency warning by moving STACK_SCREENS from Welcome.js into Confirmation.js
    
    * feat: change centering of logging option buttons and icons
    
    * toggle selected view on press
    
    * installing react-native-svg-transformers
    
    * migrating pre-existing to new lib
    
    * setting up colour switching
    
    * removed an old TODO
    
    * SVG docs
    
    * removed unnecessary prop from Selector
    
    * Add example for color change
    
    * correct color code
    
    * uninstalled old svg library
    
    * Warning regarding color switch
    
    * Prototype notifications working
    
    * Update Settings.js
    
    * Update App.js
    
    * Add AsyncStorage Service example
    
    * Changed icon
    
    * Cleaned up some of the code
    
    * Added debugger docs
    
    * Added step for launching react-native debugger
    
    * Update README.md
    
    * Update README.md
    
    * Delete OnboardingService.js
    
    * Update README.md
    
    Organize README
    
    * removed double default
    
    * fade overlay in and out
    
    * duplicate tab bar middle button in overlay
    
    * toggle overlay screen when you click middle button
    
    * animate rotation for middle tab button
    
    * new LogMultipleDates screen and new SVGs
    
    merge CalendarScreen and App.js conflicts
    
    * clean up code into component files, add comments
    
    clean up more code conflicts in App.js and CalendarScreen.js
    
    * add missing svg files, fix CalendarScreen bg color
    
    * animate rotation for middle tab button
    
    * new LogMultipleDates screen and new SVGs
    
    * clean up code into component files, add comments
    
    * fix: info navigation working and naming warnings
    
    * refactored code, fixed some styling spacing
    
    * fix: circular dependency warning by moving STACK_SCREENS from Welcome.js into Confirmation.js
    
    * feat: change centering of logging option buttons and icons
    
    * fade overlay in and out
    
    * duplicate tab bar middle button in overlay
    
    * change VectorImage elements to svgs
    
    * clean up icons, fix some start up  bugs
    
    * revert README to current develop branch
    
    Co-authored-by: Edward Han <edwardhan@HP-Printer-19YM.local>
    Co-authored-by: roylin2000 <63024415+roylin2000@users.noreply.github.com>
    Co-authored-by: Kenneth Miura <kenneth.t.miura@gmail.com>
    Co-authored-by: Chloe Zhao <59180843+yichenesia@users.noreply.github.com>
    Co-authored-by: Faye (Jingfei) Tan <jingfei.tan@mail.utoronto.ca>
    Co-authored-by: Edward Han <17802344+edwardhuahan@users.noreply.github.com>
    Co-authored-by: Roy Lin <roy.lin@mail.utoronto.ca>

--- tpp-app/src/onboarding/Password.js
+++ tpp-app/src/onboarding/Password.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, ImageBackground, TextInput, KeyboardAvoidingView } from 'react-native';
 import OnboardingBackground from '../../ios/tppapp/Images.xcassets/SplashScreenBackground.imageset/background.png'
-import { STACK_SCREENS } from './Welcome';
+import { STACK_SCREENS } from './Confirmation';
 import { BackButton } from '../home/components/BackButtonComponent';
 import { WideButton } from './components/ButtonComponents';
 import { BackButtonContainer, InputBorderContainer, PageTitleContainer } from './components/ContainerComponents';
@@ -22,8 +22,8 @@ export default function Password ({ navigation }) {
         <TextInput style={styles.input} placeholder="At least 6 characters" autoCapitalize="none" secureTextEntry={true}/>
       </InputBorderContainer>
 
-      <KeyboardAvoidingView 
-        style={{flex: 1}} 
+      <KeyboardAvoidingView
+        style={{flex: 1}}
         enabled behavior={ Platform.OS === 'ios'? 'padding': null}
         keyboardVerticalOffset={30}>
         <View style={{height: "80%"}}></View>
@@ -38,9 +38,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'stretch',
     justifyContent: 'center'
-  }, 
+  },
   input: {
-    fontFamily: "Avenir", 
+    fontFamily: "Avenir",
     fontSize: 14,
     height: 35
   }
