d523a24bfc9a74003d88eca0c1f902ce196e9b52
roylin2000 <63024415+roylin2000@users.noreply.github.com>
2022-02-05 19:18:24 -0500

    progress towards api

--- tpp-app/src/home/pages/LogMultipleDaysService.js
+++ tpp-app/src/home/pages/LogMultipleDaysService.js
@@ -1,18 +1,45 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const LogMultipleDaysService = {
-    LogMultipleDayPeriod: async function(dates){
+const FLOW_LEVEL = {
+    NONE: "NONE", 
+    LIGHT: "LIGHT", 
+    MEDIUM: "MEDIUM", 
+    HEAVY: "HEAVY", 
+    SPOTTING: "SPOTTING"
+}
+
+export const LogMultipleDaysService = {
+    LogMultipleDayPeriod: async (dates) => {
         // run this code for each value in the dates array
-        dates.map((date)=>{
+        dates.map(async (date)=>{
             const year = date.year;
             const month = date.month;
             const day = date.day;
-            
+
+            console.log(year, month, day)
+            console.log("the year " + year.toString())
             //check if year month exists
-            const data = AsyncStorage.getItem(year);
-            if (!data.contains(month)){
-                data[month] = []
-            } 
+            const data = JSON.parse(await AsyncStorage.getItem(year.toString()));
+            console.log("HEERE " + data[0][0]["Flow"]);
+            if (data == null){
+                data = [];
+            }
+
+            if (data[month-1] == null){
+                data[month-1] = [];
+            }
+
+            if (data[month-1][day-1] == null){
+                data[month-1] = {"Flow": FLOW_LEVEL.MEDIUM}
+            }else{
+                if(data[month-1][day-1]["Flow"] == null || data[month-1][day-1]["Flow"] == FLOW_LEVEL.NONE){
+                    data[month-1][day-1]["Flow"] = FLOW_LEVEL.MEDIUM;
+                }else{
+                    data[month-1][day-1]["Flow"] = FLOW_LEVEL.NONE;
+                }
+            }
+
+            console.log(JSON.stringify(data));
         })
         
     }
