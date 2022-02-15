c824b5089982a63ae5fbf052443dfe9940af5b3f
yuemily24 <76456096+yuemily24@users.noreply.github.com>
2022-02-15 13:21:04 -0500

    add backend constants, helpers, models (#81)

---
+++ tpp-app/src/home/service/utils/constants.js
@@ -0,0 +1,33 @@
+export const FLOW_LEVEL = {
+    NONE: "NONE",
+    LIGHT: "LIGHT",
+    MEDIUM: "MEDIUM",
+    HEAVY: "HEAVY",
+    SPOTTING: "SPOTTING"
+}
+
+export const MOOD_LEVEL = {
+  HAPPY: "HAPPY",
+  SAD: "SAD",
+  NEUTRAL: "NEUTRAL",
+  SICK: "SICK",
+  ANGRY: "ANGRY",
+  CUSTOM: "" // empty string
+}
+
+export const CRAMP_LEVEL = {
+  NEUTRAL: "NEUTRAL",
+  BAD: "BAD",
+  TERRIBLE: "TERRIBLE",
+  GOOD: "GOOD",
+  NONE: "NONE",
+}
+
+export const EXERCISE_TYPE = {
+    CARDIO: "CARDIO",
+    YOGA: "YOGA",
+    STRENGTH: "STRENGTH",
+    TEAM: "TEAM",
+    MARTIAL_ARTS: "MARTIAL ARTS",
+    CUSTOM: "" // empty string
+}
