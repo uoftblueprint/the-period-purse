c633b7ec4f13a6a61569204acda66c2152a65527
Alex Shih <ashih@stripe.com>
2022-03-17 19:56:29 -0700

    update button

--- tpp-app/src/onboarding/AppleSignin.js
+++ tpp-app/src/onboarding/AppleSignin.js
@@ -34,7 +34,7 @@ export default function AppleSignin() {
     return (
         <View>
             <AppleButton
-                buttonStyle={AppleButton.Style.WHITE}
+                buttonStyle={AppleButton.Style.WHITE_OUTLINE}
                 buttonType={AppleButton.Type.SIGN_IN}
                 style={styles.appleSignin}
                 onPress={() => onAppleButtonPress()}
@@ -44,26 +44,33 @@ export default function AppleSignin() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'stretch',
-      justifyContent: 'center'
-    },
-    appIcon: {
-      width: 182,
-      height: 182,
-      alignSelf: 'center'
-    },
-    titleText: {
-      textAlign: 'center',
-      fontFamily: 'Avenir',
-      fontSize: 26,
-      fontWeight: '800',
-      marginTop: 50
-    },
-    appleSignin: {
-      width: 160,
-      height: 45,
-      alignSelf: 'center'
-    }
-  });
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center'
+  },
+  appIcon: {
+    width: 182,
+    height: 182,
+    alignSelf: 'center'
+  },
+  titleText: {
+    textAlign: 'center',
+    fontFamily: 'Avenir',
+    fontSize: 26,
+    fontWeight: '800',
+    marginTop: 50
+  },
+  appleSignin: {
+    width: 160,
+    height: 45,
+    alignSelf: 'center',
+    alignItems: 'stretch', 
+    justifyContent: 'center',
+    borderRadius: 10,
+    width: 330,
+    height: 52,
+    alignSelf: 'center',
+    margin: 10
+  }
+});
