45bc98dcd4a0774551c573fce31b0b1ae76c00da
kenneth-miura <kenneth.t.miura@gmail.com>
2022-03-26 14:51:23 -0400

    semi-correct cycle component

--- tpp-app/src/home/components/CycleCard.js
+++ tpp-app/src/home/components/CycleCard.js
@@ -1,14 +1,14 @@
 import AnimatedProgressWheel from 'react-native-progress-wheel';
  import React, { useState, useEffect } from 'react';
- import {View, Text} from 'react-native';
+ import {View, Text, StyleSheet} from 'react-native';
  import CycleService from '../../services/cycle/CycleService';
  import {Card} from 'react-native-elements';
 
  function onPeriodText(periodDays){
    return (
      <View>
-       <Text>Period Day </Text>
-       <Text> {periodDays} </Text>
+       <Text style={styles.cycleText}>Period Day </Text>
+       <Text style={styles.cycleNumber}> {periodDays} </Text>
      </View>
    )
  }
@@ -16,45 +16,27 @@ import AnimatedProgressWheel from 'react-native-progress-wheel';
  function offPeriodText(daysSinceLastPeriod){
    return (
      <View>
-       <Text> {daysSinceLastPeriod} </Text>
-       <Text> Days since last period </Text>
+       <Text style={styles.cycleNumber}> {daysSinceLastPeriod} </Text>
+       <Text style={styles.cycleText}> Days since </Text>
+       <Text style={styles.cycleText}> last period </Text>
      </View>
    )
  }
 
 
- function Cycle(){
-   let [periodDays, setPeriodDays] = useState(0);
-   let [daysSinceLastPeriod, setDaysSinceLastPeriod] = useState(0);
-   let [cycleDonutPercent, setCycleDonutPercent] = useState(60);
-
-   useEffect(() =>
-   {
-     CycleService.GETPeriodDay().then(days => {
-       setPeriodDays(days);
-     });
-
-     CycleService.GETCycleDonutPercent().then(percent => {
-       //setCycleDonutPercent(percent * 100);
-     });
-
-     CycleService.GETDaysSinceLastPeriodEnd().then(days => {
-       setDaysSinceLastPeriod(days);
-     })
-   }, []);
-
-
-
-
+ function Cycle({periodDays, daysSinceLastPeriod, cycleDonutPercent}){
+   //TODO: ask someone for help with the CSS so addinig the text doesn't mess up alignment
+   //TODO: why is the text & the number not actually lined up, and also not centered within the card
   return (
   
-   <View style={{flex: 1,flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+      <View>
                    <View style={{flexDirection:'column',justifyContent:'center',alignItems:'center',alignSelf:'center'}}>
                        <View style={{flex: 1,flexDirection:'row',alignItems:'center',alignSelf:'flex-end',margin:10}}>
                          <AnimatedProgressWheel
                              size={200}
                              width={30}
                              color={'#B31F20'}
+                             fullColor={'#72C6B7'}
                              progress={cycleDonutPercent}
                              backgroundColor={'#F1F1F0'}
                          >
@@ -63,17 +45,47 @@ import AnimatedProgressWheel from 'react-native-progress-wheel';
                    </View>
 
 
-                   <View style={{justifyContent:'center',alignItems:'center',alignSelf:'center', position:'absolute'}}>
+                  <View style={{position: 'absolute', top: 0, left: 0 , right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
                      {periodDays > 0 ? onPeriodText(periodDays) : offPeriodText(daysSinceLastPeriod)}
-         </View>
-   </View>
+                  </View>
+      </View>
  )
  }
 
- export default function CycleCard(){
+ export default function CycleCard({periodDays, daysSinceLastPeriod, cycleDonutPercent}){
    return (
-       <View>
-       <Cycle/>
-       </View>
+        <Card containerStyle={styles.card}>
+          <Cycle periodDays={periodDays} daysSinceLastPeriod={daysSinceLastPeriod} cycleDonutPercent={cycleDonutPercent}/>
+        </Card>
    );
-   }
+}
+
+const styles = StyleSheet.create({
+  card:{
+    borderRadius: 12,
+    width: 343,
+    height: 298
+  },
+  cycleText: {
+    fontFamily: "Avenir",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: "500",
+    lineHeight: 22,
+    letterSpacing: -0.30000001192092896,
+    textAlign: "center",
+    color: "#6D6E71"
+  },
+  cycleNumber: {
+    fontFamily: "Avenir",
+    fontSize: 50,
+    fontStyle: "normal",
+    fontWeight: "900",
+    lineHeight: 68,
+    letterSpacing: -0.30000001192092896,
+    textAlign: "center",
+    color: "#B31F20"
+  }
+
+
+});
--- tpp-app/src/home/pages/CycleScreen.js
+++ tpp-app/src/home/pages/CycleScreen.js
@@ -1,6 +1,6 @@
 
 import AnimatedProgressWheel from 'react-native-progress-wheel';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {StyleSheet, Text, ImageBackground, SafeAreaView} from 'react-native';
 import CycleCard from '../components/CycleCard';
 import background from '../../../ios/tppapp/Images.xcassets/SplashScreenBackground.imageset/watercolor-background.png';
@@ -8,7 +8,7 @@ import {Card} from 'react-native-elements';
 
 import BloodDrop from '../../../ios/tppapp/Images.xcassets/icons/flow_with_heart.svg';
 import Calendar from '../../../ios/tppapp/Images.xcassets/icons/menstruation_calendar.svg';
-import FlowIcon from "../../../ios/tppapp/Images.xcassets/icons/flow.svg";
+ import CycleService from '../../services/cycle/CycleService';
 
 function InfoCard(props){
   return (
@@ -24,16 +24,58 @@ function InfoCard(props){
 }
 
 export default function CycleScreen (){
+
+  let [avgPeriodLength, setAvgPeriodLength] = useState(0);
+  let [avgCycleLength, setAvgCycleLength] = useState(0);
+  let [periodDays, setPeriodDays] = useState(0);
+  let [daysSinceLastPeriod, setDaysSinceLastPeriod] = useState(0);
+  let [cycleDonutPercent, setCycleDonutPercent] = useState(20);
+
+  useEffect(() => {
+     CycleService.GETPeriodDay().then(days => {
+       setPeriodDays(days);
+     });
+
+     CycleService.GETCycleDonutPercent().then(percent => {
+       //setCycleDonutPercent(percent * 100);
+     });
+
+     CycleService.GETDaysSinceLastPeriodEnd().then(days => {
+       setDaysSinceLastPeriod(days);
+     });
+
+     CycleService.GETAveragePeriodLength().then(numDays => {
+       if(numDays){
+        setAvgPeriodLength(numDays);
+       }
+       else {
+         setAvgPeriodLength(0);
+       }
+     });
+     
+     CycleService.GETAverageCycleLength().then(numDays => {
+       if(numDays){
+        setAvgCycleLength(numDays);
+       }
+       else {
+         setAvgCycleLength(0);
+       }
+     });
+
+  }, []);
+{periodDays, daysSinceLastPeriod, cycleDonutPercent}
+
   return (
     <ImageBackground source={background} style={styles.container}>    
-      <SafeAreaView style={styles.rowContainer}>
-        <InfoCard header="Average period length" days={7} backgroundColor="#FFDBDB">
+      <CycleCard periodDays={periodDays} daysSinceLastPeriod={daysSinceLastPeriod} cycleDonutPercent={cycleDonutPercent}/>
+        <SafeAreaView style={styles.rowContainer}>
+
+        <InfoCard header="Average period length" days={avgPeriodLength} backgroundColor="#FFDBDB">
           <BloodDrop fill="red"/>
         </InfoCard>
-        <InfoCard header="Average cycle length" days={7} backgroundColor="#B9E0D8">
+        <InfoCard header="Average cycle length" days={avgCycleLength} backgroundColor="#B9E0D8">
           <Calendar fill="red"/>
         </InfoCard>
-
       </SafeAreaView>
     </ImageBackground>
 
