60da3402dd49b83311c3d5a3f1300bfe814b77ef
faye.tan <jingfei.tan@mail.utoronto.ca>
2022-04-16 15:46:22 -0400

    Fix for null case LogSymptomsScreen.js

--- tpp-app/src/home/pages/LogSymptomsScreen.js
+++ tpp-app/src/home/pages/LogSymptomsScreen.js
@@ -206,7 +206,8 @@ export default function LogSymptomsScreen({ navigation, route }) {
       .then(() => {
         let inputData = {}
         inputData[getISODate(selectedDate)] = {
-          symptoms: submitSymp
+          // submitSymp may be null, in that case pass back blank Symptoms object
+          symptoms: submitSymp ? submitSymp : new Symptoms()
         }
         navigation.navigate(STACK_SCREENS.CALENDAR_PAGE, {inputData: inputData})
         // navigation.goBack(isDirty);
