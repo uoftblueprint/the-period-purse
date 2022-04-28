e00cd8ae54226482cb4a68513d97e22ea33e1080
Annie <liuzr.annie@gmail.com>
2022-04-27 22:05:12 -0400

    Use effect

--- tpp-app/src/pages/DYKInfo.js
+++ tpp-app/src/pages/DYKInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {StyleSheet, Text, View, Image, SafeAreaView, ImageBackground} from 'react-native';
 import PadImageHappy from 'tpp-app/ios/tppapp/Images.xcassets/InfoPageImages/pad-3-2x.png';
 import { BackButton } from '../home/components/BackButtonComponent';
@@ -8,8 +8,34 @@ import { getFullCurrentDateString } from "../services/utils/helpers.js"
 import dykData from "../pages/DYKFacts.json"
 
 export default function DidYouKnow({ navigation }) {
-    let fact = getFact()
-    console.log(fact);
+    const [factCycleArray, setFactCycleArray] = useState([]);
+    useEffect(() => {
+        async function retrieveFactCycle() {
+            var factArray = await GETFactCycle()
+            setFactCycleArray(factArray);
+            console.log(`${factArray}`)
+
+            if (!factArray){
+                await POSTFactCycle().then(async () => {
+                    factArray = await GETFactCycle();
+                    console.log(`${factArray}`)
+                    setFactCycleArray(factArray);
+                })
+            }
+        if (getFullCurrentDateString() != factArray[0]) {
+            POSTFactCycle().then(async () => {
+            factArray = await GETFactCycle();
+            console.log(`${factArray}`)
+            setFactCycleArray(factArray)
+        });
+        }    
+        }
+        retrieveFactCycle()
+    }, [])
+
+    let fact = dykData[factCycleArray[1]]
+    console.log(`This is factCycleArray: ${factCycleArray[1]}`)
+    console.log(`This is fact: ${fact}`)
     return (
         <ImageBackground source={OnboardingBackground} style={styles.container}>
             <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -35,27 +61,27 @@ export default function DidYouKnow({ navigation }) {
  * Retrieves the fact that the user is supposed to see that day
  * @returns a string of the relevant fact
  */
- const getFact = async () => {
-    // try to get the fact cycle array first
-    var factArray = await GETFactCycle();
+//  const getFact = async () => {
+//     // try to get the fact cycle array first
+//     var factArray = await GETFactCycle();
 
-    // if the array is null, then this means we haven't initiatlized the fact cycle array
-    if (factArray == null) {
-        // initialize the fact cycle with POSTFactCycle
-        await POSTFactCycle().then( async () => {
-            factArray = await GETFactCycle();
-        });
+//     // if the array is null, then this means we haven't initiatlized the fact cycle array
+//     if (factArray == null) {
+//         // initialize the fact cycle with POSTFactCycle
+//         await POSTFactCycle().then( async () => {
+//             factArray = await GETFactCycle();
+//         });
         
-    }
-    // if today's date and the stored date don't match, update
-    if (getFullCurrentDateString() != factArray[0]) {
-        POSTFactCycle().then(() => {
-            factArray = GETFactCycle();
-        });
-    }
+//     }
+//     // if today's date and the stored date don't match, update
+//     if (getFullCurrentDateString() != factArray[0]) {
+//         POSTFactCycle().then(() => {
+//             factArray = GETFactCycle();
+//         });
+//     }
 
-    return dykData[factArray[1]]
-}
+//     return dykData[factArray[1]]
+// }
 
 const styles = StyleSheet.create({
     container: {
